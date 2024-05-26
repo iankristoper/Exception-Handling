ad322134ec7bc6776af5d2f09fa5703e79fd4cca
iankristoper <iankristoper26@gmail.com>
2024-05-26 17:53:26 +0800

    initial commit

---
+++ src/main/java/com/ian/divide_by_zero/Divide_by_Zero.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ian.divide_by_zero;
+
+/**
+ *
+ * @author Ian Tagano
+ */
+public class Divide_by_Zero {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
